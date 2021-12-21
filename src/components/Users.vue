6025eb7f697445682de5ecfd5a17f796e6705fd0
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2021-12-21 10:14:28 +0100

    fin exo

---
+++ src/components/Users.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h3>Liste des utilisateurs :</h3>
+    <table class="table bg-light">
+      <thead>
+        <tr class="bg-info text-light">
+          <th scope="col">Identifiant</th>
+          <th scope="col">Nom</th>
+          <th scope="col">Email</th>
+          <th scope="col">Ville</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+<script>
+  import axios from 'axios';  export default {
+    name: 'Users',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
+  }
+</script>
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
